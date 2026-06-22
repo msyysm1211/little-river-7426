17557fb93cb4fd65cf3e62c174d53ca696fb64ce
wushan2025-3 <wushan2025_3@126.com>
2026-06-22 13:31:18 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,8 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   images: {
     unoptimized: false,
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [{ hostname: 'img.alicdn.com' }],
+    formats: ['image/avif', 'image/webp', 'image/png'],
+    remotePatterns: [{ hostname: 'help-static-aliyun-doc.aliyuncs.com'}],
   },
 };
 
